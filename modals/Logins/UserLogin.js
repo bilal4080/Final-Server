0cf99e4c846cefdab0e94c90faa797c6a1a906a0
Bilal <68295612+bilal4080@users.noreply.github.com>
2025-01-21 16:17:37 +0500

    ddd

--- modals/Logins/UserLogin.js
+++ modals/Logins/UserLogin.js
@@ -1,10 +1,20 @@
-
 const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  status: { type: Boolean, default: true },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+    trim: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  status: { 
+    type: Boolean, 
+    default: false 
+  },
 });
 
 const User = mongoose.model('IMDFXUSER', userSchema);
